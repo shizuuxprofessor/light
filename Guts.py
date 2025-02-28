5c7dcc3f5c839dc5904a114fb99aa6fcdcef399b
shizuuxprofessor <proffesorddos@gmail.com>
2025-02-28 11:38:23 +0530

    Update Guts.py

--- Guts.py
+++ Guts.py
@@ -5,8 +5,6 @@ import subprocess
 import datetime
 import os
 
-from keep_alive import keep_alive
-keep_alive()
 # Insert your Telegram bot token here
 bot = telebot.TeleBot('7675494292:AAH6MvE49bACykXD5DtWNcadgxGAnYQN5W8')
 
